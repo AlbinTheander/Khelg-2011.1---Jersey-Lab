8024186581a0276c42940536ac82728b9578c378
Albin Theander <Albin.Theander@jayway.com>
2011-03-12 19:16:28 +0100

    Added method for retrieving single book

--- test-app/src/main/java/com/jayway/jerseylab/Books.java
+++ test-app/src/main/java/com/jayway/jerseylab/Books.java
@@ -2,6 +2,7 @@ package com.jayway.jerseylab;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -10,8 +11,14 @@ public class Books {
 	
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	public String allBooks() {
+	public String getAllBooks() {
 		return "This is a list of books";
+	}
+	
+	@Path("/{bookId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getBook(@PathParam("bookId") String id) {
+		return "Book with id: " + id;
 	}
 
 }
