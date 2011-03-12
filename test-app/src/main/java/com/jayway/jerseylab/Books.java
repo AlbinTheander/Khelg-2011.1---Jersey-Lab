87c4f13416d153ff0b4d994995221f8e27748db8
Albin Theander <Albin.Theander@jayway.com>
2011-03-12 19:51:43 +0100

    Added search method to Books

--- test-app/src/main/java/com/jayway/jerseylab/Books.java
+++ test-app/src/main/java/com/jayway/jerseylab/Books.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/books")
@@ -15,10 +16,19 @@ public class Books {
 		return "This is a list of books";
 	}
 	
+
+	@GET
 	@Path("/{bookId}")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getBook(@PathParam("bookId") String id) {
 		return "Book with id: " + id;
+	}
+	
+	@GET
+	@Path("/search")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String search(@QueryParam("title") String query) {
+		return "Book search query: " + query;
 	}
 
 }
